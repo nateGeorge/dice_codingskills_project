9104770b476a95ed7ed11815fec90d9f3c6f0bb1
wordsforthewise <wordsforthewise@gmail.com>
2017-01-31 21:17:14 -0700

    FEAT: add xgboost insall

--- install_script.sh
+++ install_script.sh
@@ -3,7 +3,12 @@ sudo apt-get update
 sudo apt-get upgrade -y
 git clone https://github.com/nateGeorge/dice_codingskills_project.git
 
-sudo apt-get install python-minimal python-pip mongodb python-matplotlib -y
+sudo apt-get install python-minimal python-pip mongodb python-matplotlib python-setuptools -y
 pip -U matplotlib
 pip install --upgrade pip
 sudo pip install sklearn numpy pandas scipy pymongo flask requests bs4 lxml ipython seaborn tqdm bokeh
+
+# install xgboost
+git clone --recursive https://github.com/dmlc/xgboost
+cd xgboost; make -j4
+cd python-package; sudo python setup.py install
