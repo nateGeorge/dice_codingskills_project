170068229b24482be89d720d65e5456c91593711
nateGeorge <wordsforthewise@gmail.com>
2019-03-12 13:51:01 -0600

    add error from mongodump

---
+++ dice_code/scraping_error.py
@@ -0,0 +1,7 @@
+QXcbConnection: XCB error: 148 (Unknown), sequence: 411, resource id: 0, major code: 140 (Unknown), minor code: 20
+QXcbConnection: XCB error: 148 (Unknown), sequence: 425, resource id: 0, major code: 140 (Unknown), minor code: 20
+QXcbConnection: XCB error: 148 (Unknown), sequence: 437, resource id: 0, major code: 140 (Unknown), minor code: 20
+QXcbConnection: XCB error: 148 (Unknown), sequence: 1380, resource id: 0, major
+code: 140 (Unknown), minor code: 20
+QXcbConnection: XCB error: 148 (Unknown), sequence: 1949, resource id: 0, major
+code: 140 (Unknown), minor code: 20
